b976999bbde33ac086e2f12a5a14a34de31ceba1
eden <ecoding@pm.me>
2024-10-10 11:43:59 -0700

    Question 1 Done

--- main.py
+++ main.py
@@ -3,19 +3,39 @@
 import numpy
 import matplotlib
 import pandas
-
-#Variables
-p0 = 0.4
-e0 = 0.02
-e1 = 0.015
+import random
 
 
 #Problem 1
-def prob1():
-    print("ah")
+def prob1() -> bool:
+    #Variables
+    p0 = 0.4
+    e0 = 0.02
+    e1 = 0.015
+    signalS = 0
+    signalR = 0
+    m = random.uniform(0.00, 1.00)
+    t = random.uniform(0.015, 0.02)
+    result = True
+    
+    #Assign Variable S
+    if(m <= p0):
+        signalS = 0
+    else:
+        signalS = 1
+
+    if ((signalS == 0 and t <= e1) or (signalS == 1 and t > e1)):
+        signalR = 1
+        result = True
+        return result
+    elif ((signalS == 0 and t > e0) or (signalS == 1 and t <= e1)):
+        signalR = 0
+        result = False
+        return result
 
 #Problem 2
 def prob2():
+
     print("oh")
 
 #Problem 3
@@ -25,3 +45,11 @@ def prob3():
 #Problem 4
 def prob4():
     print("uh")
+
+def main():
+    count = 0
+    for i in range(10,000):
+        result = prob1()
+        if(result == False):
+            count += 1
+    print(count)
