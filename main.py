54c2b83d997371685a4e48d64bf88ac3ab473076
eden <ecoding@pm.me>
2024-10-09 18:02:29 -0700

    Actually started working on this

--- main.py
+++ main.py
@@ -2,9 +2,17 @@
 #Project 2
 
 #Problem 1
+def prob1():
+    print("ah")
 
 #Problem 2
+def prob2():
+    print("oh")
 
 #Problem 3
+def prob3():
+    print("eh")
 
 #Problem 4
+def prob4():
+    print("uh")
