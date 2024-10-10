6d227edd6684c213b9dbc0ca0fa97920a4026e42
eden <ecoding@pm.me>
2024-10-10 11:58:14 -0700

    Problem 2 mostly done

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def prob1() -> bool:
     signalR = 0
     m = random.uniform(0.00, 1.00)
     t = random.uniform(0.015, 0.02)
-    result = True
     
     #Assign Variable S
     if(m <= p0):
@@ -22,34 +21,67 @@ def prob1() -> bool:
 
     if ((signalS == 0 and t <= e1) or (signalS == 1 and t > e1)):
         signalR = 1
-        result = True
-        return result
+        return True
     elif ((signalS == 0 and t > e0) or (signalS == 1 and t <= e1)):
         signalR = 0
-        result = False
-        return result
+        return False
 
-#Problem 2 - N/A
-def prob2():
-    print("oh")
+#Problem 2 - WIP
+def prob2() -> bool:
+    #Variables
+    p0 = 0.4
+    e0 = 0.02
+    e1 = 0.015
+    signalS = 0
+    signalR = 0
+    m = random.uniform(0.00, 1.00)
+    t = random.uniform(0.015, 0.02)
+    
+    #Assign Variable S
+    if(m <= p0):
+        signalS = 0
+    else:
+        signalS = 1
+
+    if ((signalS == 0 and t <= e1) or (signalS == 1 and t > e1)):
+        signalR = 1
+    elif ((signalS == 0 and t > e0) or (signalS == 1 and t <= e1)):
+        signalR = 0
+
+    if(signalR == signalS):
+        return True
+    else:
+        return False
 
 #Problem 3 - N/A
-def prob3():
+def prob3() -> bool:
     print("eh")
 
 #Problem 4 - N/A
-def prob4():
+def prob4() -> bool:
     print("uh")
 
 def main():
     #Problem 1
     count = 0
-    for i in range(10,000):
+    for i in range(10000):
         result = prob1()
         if(result == False):
             count += 1
-            print(count)
-        else:
-            print("True result")
+            
+    print("Problem 1: p=",count, "/10,000")
 
+    #Problem 2
+    count = 0
+    for i in range(100000):
+        result = prob2()
+        if(result == True):
+            count+=1
+    
+    print("Problem 2: p=",count,"/100,000")
+
+    #Problem 3
+
+    #Problem 4
+   
 main()
