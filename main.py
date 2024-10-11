ccf7aacbd81ca98984a2a2200336ea892734653f
eden <ecoding@pm.me>
2024-10-11 14:37:26 -0700

    Problem 3

--- main.py
+++ main.py
@@ -56,9 +56,32 @@ def prob2() -> bool:
     else:
         return False
 
-#Problem 3 - N/A
+#Problem 3 - DONE
 def prob3() -> bool:
-    print("eh")
+    #Variables
+    p0 = 0.4
+    e0 = 0.02
+    e1 = 0.015
+    signalS = 0
+    signalR = 0
+    m = random.uniform(0.00, 1.00)
+    t = random.uniform(0.015, 0.02)
+
+    #Assign Variable S
+    if(m <= p0):
+        signalS = 0
+    else:
+        signalS = 1
+
+    if ((signalS == 0 and t <= e0) or (signalS == 1 and t > e1)):
+        signalR = 1
+    elif ((signalS == 0 and t > e0) or (signalS == 1 and t <= e1)):
+        signalR = 0
+
+    if(signalR != signalS):
+        return False
+    else:
+        return True
 
 #Problem 4 - N/A
 def prob4() -> bool:
@@ -85,6 +108,13 @@ def main():
     print("Problem 2: p =",count,"/ 100000")
 
     #Problem 3
+    count = 0
+    for i in range(10000):
+        result = prob3()
+        if(result == True):
+            count+=1
+
+    print("Problem 3: p =",count,"/ 10000")
 
     #Problem 4
 
