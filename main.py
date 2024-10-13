e90f779dc76b99354d7a2f427a07dd3eef02243d
eden <ecoding@pm.me>
2024-10-13 15:58:14 -0700

    Started working on Problem 4

--- main.py
+++ main.py
@@ -83,9 +83,32 @@ def prob3() -> bool:
     else:
         return True
 
-#Problem 4 - N/A
+#Problem 4 - WIP
 def prob4() -> bool:
-    print("uh")
+    #Variables
+    p0 = 0.4
+    e0 = 0.02
+    e1 = 0.015
+    signalS = 0
+    signalR = 0
+    m = random.uniform(0.00, 1.00)
+    t = random.uniform(0.015, 0.02)
+
+    #Assign Variable S
+    if(m <= p0):
+        signalS = 0
+    else:
+        signalS = 1
+
+    if ((signalS == 0 and t <= e0) or (signalS == 1 and t > e1)):
+        signalR = 1
+    elif ((signalS == 0 and t > e0) or (signalS == 1 and t <= e1)):
+        signalR = 0
+
+    if(signalR != signalS):
+        return False
+    else:
+        return True
 
 #Main function
 def main():
